6c4e6605ae8aab264ab0b60648c4bbf53e0b7ec8
kuldeep <kuldeep5534@gmail.com>
2015-06-29 16:43:34 +0530

    Refactored.

--- Web Page Download/src/in/interview/google/Downloader.java
+++ Web Page Download/src/in/interview/google/Downloader.java
@@ -5,22 +5,78 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Iterator;
 
 /**
- * 
+ * Download related utilities for web crawler.
  * 
  * @author kuldeep
  */
 public class Downloader {
 
-	public static void download(String url) throws IOException {
+	/**
+	 * @param url
+	 * 			url of a web page to download
+	 * @return {@link Iterator} over lines in file.
+	 * @throws IOException
+	 */
+	public Iterator<String> download(String url) throws IOException {
 		
 		InputStream inputStream = new URL(url).openStream();
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 		
-		String line;
-		while ((line = bufferedReader.readLine()) != null) {
-			System.out.println(line);
+		return new LineIterator<String>(bufferedReader);
+	}
+	
+	/**
+	 * Iterator over lines in {@link BufferedReader}.
+	 * 
+	 * @author kuldeep
+	 *
+	 * @param <String>
+	 */
+	@SuppressWarnings("hiding")
+	private class LineIterator<String> implements Iterator<String> {
+
+		/**
+		 * Look ahead string.
+		 */
+		private String lookAhead;
+		
+		/**
+		 * Buffered reader to get strings.
+		 */
+		private BufferedReader reader;
+		
+		public LineIterator(BufferedReader reader) throws IOException {
+			this.reader = reader;
+			loadLookAhead();
+		}
+		
+		/**
+		 * Load next string.
+		 * 
+		 * @throws IOException
+		 */
+		@SuppressWarnings("unchecked")
+		private void loadLookAhead() throws IOException {
+			this.lookAhead = (String) reader.readLine();
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return lookAhead != null;
+		}
+
+		@Override
+		public String next() {
+			String toReturn = lookAhead;
+			try {
+				loadLookAhead();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return toReturn;
 		}
 		
 	}
--- Web Page Download/test/in/interview/google/DownloaderTest.java
+++ Web Page Download/test/in/interview/google/DownloaderTest.java
@@ -1,15 +1,27 @@
 package in.interview.google;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.junit.Test;
 
+/**
+ * Junit test for {@link Downloader}.
+ * 
+ * @author kuldeep
+ *
+ */
 public class DownloaderTest {
 
 	@Test
 	public void testDownload() throws IOException {
 		
-		Downloader.download("https://en.wikipedia.org/wiki/World_War_II");
+		Downloader downloader = new Downloader();
+		Iterator<String> itr = downloader.download("https://en.wikipedia.org/wiki/Main_Page");
+		while (itr.hasNext()) {
+			String string = itr.next();
+			System.out.println(string);
+		}
 	}
 
 }
