7f12b64cd9b90bb337713f696abe3a635130ee96
kuldeep <kuldeep5534@gmail.com>
2015-06-29 18:16:19 +0530

    Crawler added. Not implemented yet.

---
+++ Web Page Download/src/in/interview/google/Crawler.java
@@ -0,0 +1,26 @@
+package in.interview.google;
+
+import java.util.Iterator;
+
+/**
+ * Crawler to iterate over URLs.
+ * 
+ * @author kuldeep
+ */
+public class Crawler implements Iterator<String>{
+
+	private String seedUrl;
+	
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- Web Page Download/test/in/interview/google/DownloaderTest.java
+++ Web Page Download/test/in/interview/google/DownloaderTest.java
@@ -2,6 +2,9 @@ package in.interview.google;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 
@@ -14,14 +17,41 @@ import org.junit.Test;
 public class DownloaderTest {
 
 	@Test
-	public void testDownload() throws IOException {
+	public void testDownload() {
 		
 		Downloader downloader = new Downloader();
-		Iterator<String> itr = downloader.download("https://en.wikipedia.org/wiki/Main_Page");
-		while (itr.hasNext()) {
-			String string = itr.next();
-			System.out.println(string);
+		Iterator<String> itr = null;
+		
+		String regex = "(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+		Pattern pattern = Pattern.compile(regex);
+		
+		LinkedList<String> list = new LinkedList<String>();
+//		list.add("https://en.wikipedia.org/wiki/Main_Page");
+		list.add("https://www.google.co.in");
+		int count = 1000;
+		
+		outerloop:
+		while (!list.isEmpty()) {
+			try {
+				itr = downloader.download(list.poll());
+			} catch (IOException e) {
+				continue;
+			}
+			while (itr.hasNext()) {
+				String line = itr.next();
+				Matcher matcher = pattern.matcher(line);
+				while (matcher.find()) {
+					String url = matcher.group();
+					System.out.println(count + "\t" + url);
+					list.add(url);
+					count--;
+					if (count <= 0) {
+						break outerloop;
+					}
+				}
+			}
 		}
+		
 	}
 
 }
